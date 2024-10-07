06ce2338fa745171e3dda6515c9614d424da7e32
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-07 20:59:24 +0200

    Initial Commit

---
+++ src/main/java/fhv/team11/project/ems/user/UserRepository.java
@@ -0,0 +1,9 @@
+package fhv.team11.project.ems.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByEmail(String email);
+}
