eab1390614285b4f354aba414e264925c7259123
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-07 20:59:12 +0200

    Initial Commit

---
+++ src/main/java/fhv/team11/project/ems/security/jwt/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package fhv.team11.project.ems.security.jwt;
+
+import fhv.team11.project.ems.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public JwtUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
