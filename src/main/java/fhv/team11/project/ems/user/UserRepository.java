9c24e343ba8f3752b22f6d97705f6d9b82be7408
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-18 13:24:56 +0200

    moved from JpaRepository to Jdbc

--- src/main/java/fhv/team11/project/ems/user/UserRepository.java
+++ src/main/java/fhv/team11/project/ems/user/UserRepository.java
@@ -1,9 +1,42 @@
 package fhv.team11.project.ems.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    public Optional<User> findByEmail(String email);
+@Repository
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // Find user by email (as a replacement for findByEmail in JPA)
+    public Optional<UserModel> findByEmail(String email) {
+        String sql = "SELECT * FROM users WHERE email = ?";
+        return jdbcTemplate.query(sql, new Object[]{email}, new UserRowMapper())
+                .stream()
+                .findFirst();
+    }
+
+    // Save a new user to the database
+    public void save(UserModel user) {
+        String sql = "INSERT INTO users (email, password, authority) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, user.getEmail(), user.getPassword(), user.getAuthority().ordinal());
+    }
+
+    // Update an existing user
+    public void update(UserModel user) {
+        String sql = "UPDATE users SET email = ?, password = ?, authority = ? WHERE id = ?";
+        jdbcTemplate.update(sql, user.getEmail(), user.getPassword(), user.getAuthority().ordinal(), user.getId());
+    }
+
+    // Delete a user by ID
+    public void deleteById(Long id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
 }
