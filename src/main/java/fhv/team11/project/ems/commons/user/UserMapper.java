fcf336f27c462d2951cde2ad9773e05e92de79a8
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-20 02:58:48 +0200

    implemented IMapper in UserMapper

--- src/main/java/fhv/team11/project/ems/commons/user/UserMapper.java
+++ src/main/java/fhv/team11/project/ems/commons/user/UserMapper.java
@@ -1,26 +1,39 @@
 package fhv.team11.project.ems.commons.user;
 
+import fhv.team11.project.ems.commons.mapper.IMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserMapper {
+public class UserMapper implements IMapper<UserEntity, User> {
 
-    public static User toViewModel(UserEntity userModel) {
+    @Override
+    public User toDomainModel(UserEntity userEntity) {
         User user = new User();
-        user.setEmail(userModel.getEmail());
-        user.setAuthority(userModel.getAuthority());
+        user.setEmail(userEntity.getEmail());
+        user.setAuthority(userEntity.getAuthority());
         return user;
     }
 
     /**
      *
-     * @param user
-     * @return returns the user model corresponding to the ViewModel. But the password and Id fields will be missing
+     * @Warning This implementation does not provide the password and Id fields.
      */
-    public static UserEntity toModel(User user) {
-        UserEntity userModel = new UserEntity();
-        userModel.setEmail(user.getEmail());
-        userModel.setAuthority(user.getAuthority());
-        return userModel;
+    @Override
+    public UserEntity toEntity(User user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(user.getEmail());
+        userEntity.setAuthority(user.getAuthority());
+        return userEntity;
+    }
+
+    @Override
+    public UserEntity updateEntity(UserEntity userEntity, User user) {
+        if (userEntity == null || user == null) {
+            return userEntity;
+        }
+
+        userEntity.setEmail(user.getEmail());
+        userEntity.setAuthority(user.getAuthority());
+        return userEntity;
     }
 }
