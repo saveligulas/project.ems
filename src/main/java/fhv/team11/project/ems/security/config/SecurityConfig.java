1c4d84c985cb1d9cdab3e69e80ddda70ef574d45
saveligulas <saveli.gulas@gmail.com>
2024-10-21 13:21:38 +0200

    changed to permitAll

--- src/main/java/fhv/team11/project/ems/security/config/SecurityConfig.java
+++ src/main/java/fhv/team11/project/ems/security/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                         .requestMatchers("error")
                         .permitAll()
                         .anyRequest()
-                        .authenticated()
+                        .permitAll()
         );
         http.sessionManagement(
                 session -> session
