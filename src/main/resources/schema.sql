0b4b46c59f9dc960d48c1f4d8cdb462136130b02
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-25 01:51:35 +0200

    added seperate table for roles

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,10 @@
-CREATE TABLE IF NOT EXISTS user (
-                                     id BIGSERIAL PRIMARY KEY,
-                                     email VARCHAR(255) NOT NULL UNIQUE,
-                                     password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS "user" (
+    id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "user_roles" (
+    user_id BIGSERIAL NOT NULL,
+    role_ordinal INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE
 );
