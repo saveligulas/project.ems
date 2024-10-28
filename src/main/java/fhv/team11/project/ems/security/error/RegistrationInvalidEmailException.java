1a66700d3cde8d66668c1272da860f52f8f0309f
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-28 23:16:22 +0100

    removed unused Exceptions

--- src/main/java/fhv/team11/project/ems/security/error/RegistrationInvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package fhv.team11.project.ems.security.error;
-
-public class RegistrationInvalidEmailException extends RuntimeException {
-    public RegistrationInvalidEmailException() {
-        super("Invalid email address provided");
-    }
-}
