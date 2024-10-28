3229dbaac6d66c8cf85b71589846be153c6cd959
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-29 00:41:11 +0100

    added Sessions if required

--- src/main/java/fhv/team11/project/ems/security/config/SecurityConfig.java
+++ src/main/java/fhv/team11/project/ems/security/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
         );
         http.sessionManagement(
                 session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
         );
         http.userDetailsService(userDetailsService);
         http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
