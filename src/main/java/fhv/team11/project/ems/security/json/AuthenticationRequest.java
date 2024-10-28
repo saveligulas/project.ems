0b8a168fe7323e69e4bb2fef8088ef11852d49b9
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-29 00:20:04 +0100

    added Validation to AuthenticationRequest

--- src/main/java/fhv/team11/project/ems/security/json/AuthenticationRequest.java
+++ src/main/java/fhv/team11/project/ems/security/json/AuthenticationRequest.java
@@ -1,11 +1,16 @@
 package fhv.team11.project.ems.security.json;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationRequest {
+    @NotBlank(message = "Please enter an email address")
     private String email;
+    @NotBlank(message = "Please enter a password")
     private String password;
 }
