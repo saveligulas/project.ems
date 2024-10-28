963be57a6c268bacd9b7dbe1d1a8f178674494be
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-28 23:17:32 +0100

    added RegistrationError sub class

--- src/main/java/fhv/team11/project/ems/security/error/RegistrationEmailAlreadyRegisteredException.java
+++ src/main/java/fhv/team11/project/ems/security/error/RegistrationEmailAlreadyRegisteredException.java
@@ -1,6 +1,6 @@
 package fhv.team11.project.ems.security.error;
 
-public class RegistrationEmailAlreadyRegisteredException extends RuntimeException {
+public class RegistrationEmailAlreadyRegisteredException extends RegistrationError {
     public RegistrationEmailAlreadyRegisteredException() {
         super("User with email already registered");
     }
