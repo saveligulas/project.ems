22318ea26c255979d6c2c3cb82d02a269391379a
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-29 00:40:30 +0100

    code cleanup and email validation

--- src/main/java/fhv/team11/project/ems/security/json/AuthenticationRequest.java
+++ src/main/java/fhv/team11/project/ems/security/json/AuthenticationRequest.java
@@ -1,5 +1,6 @@
 package fhv.team11.project.ems.security.json;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,8 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
+
     @NotBlank(message = "Please enter an email address")
+    @Email(message = "Please enter a valid email address")
     private String email;
+
     @NotBlank(message = "Please enter a password")
     private String password;
 }
