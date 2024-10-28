5abb0fb448259f89ecc3e358c10da1fd1d8c12e7
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-28 23:16:40 +0100

    added Validation Steps

---
+++ src/main/java/fhv/team11/project/ems/commons/validation/order/FirstValidation.java
@@ -0,0 +1,5 @@
+package fhv.team11.project.ems.commons.validation.order;
+
+public interface FirstValidation {
+}
+
---
+++ src/main/java/fhv/team11/project/ems/commons/validation/order/FourthValidation.java
@@ -0,0 +1,4 @@
+package fhv.team11.project.ems.commons.validation.order;
+
+public interface FourthValidation {
+}
