cdea9166e3ab4b7bc9441166025a704d9d2c19ea
Saveli Gulas <saveli.gulas@gmail.com>
2024-10-31 14:56:02 +0100

    added AddressRepository

---
+++ src/main/java/fhv/team11/project/ems/commons/address/AddressRepository.java
@@ -0,0 +1,10 @@
+package fhv.team11.project.ems.commons.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
