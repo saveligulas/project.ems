8d8a32a76fd9fc4e2cacdac5205f4f462d1cedae
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-03 16:45:32 +0100

    .

--- src/main/java/fhv/team11/project/ems/commons/address/CustomAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package fhv.team11.project.ems.commons.address;
-
-import java.util.Optional;
-
-public interface CustomAddressRepository {
-    Optional<Address> findById();
-    void persist(Address address);
-}
--- src/main/java/fhv/team11/project/ems/events/service/BlueprintDTO.java
+++
@@ -1,4 +0,0 @@
-package fhv.team11.project.ems.events.service;
-
-public class BlueprintDTO {
-}
