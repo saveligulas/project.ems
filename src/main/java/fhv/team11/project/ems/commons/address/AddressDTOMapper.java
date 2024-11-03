f37f06d6de6b39fd02deeb87a453b2d9b0cee5ed
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-04 00:28:51 +0100

    .

--- src/main/java/fhv/team11/project/ems/commons/address/AddressDTOMapper.java
+++ src/main/java/fhv/team11/project/ems/commons/address/AddressDTOMapper.java
@@ -1,15 +1,12 @@
 package fhv.team11.project.ems.commons.address;
 
-public class AddressDTOMapper {
-    public static Address getAddress(AddressDTO dto) {
-        Address address = new Address();
-        address.setCountry(dto.getCountry());
-        address.setRegion(dto.getRegion());
-        address.setCity(dto.getCity());
-        address.setZip(dto.getZip());
-        address.setStreet(dto.getStreet());
-        address.setNumber(dto.getHouseNumber());
-        address.setOptionalText(dto.getOptionalText());
-        return address;
-    }
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AddressDTOMapper {
+    AddressDTOMapper INSTANCE = Mappers.getMapper(AddressDTOMapper.class);
+
+    Address toEntity(AddressDTO addressDTO);
+    AddressDTO toDTO(Address address);
 }
