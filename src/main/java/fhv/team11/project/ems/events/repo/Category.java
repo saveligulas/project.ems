3b02232a6853d61a7971ac682d9379f2ea7e502f
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-04 00:29:13 +0100

    .

--- src/main/java/fhv/team11/project/ems/events/repo/Category.java
+++ src/main/java/fhv/team11/project/ems/events/repo/Category.java
@@ -1,5 +1,15 @@
 package fhv.team11.project.ems.events.repo;
 
 public enum Category {
-    SKI
+    SKI("Ski Courses");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
