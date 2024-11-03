9f30b4b21a2d50f09129a2f99703dfe6286248d6
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-03 16:42:18 +0100

    .

--- src/main/java/fhv/team11/project/ems/commons/address/Address.java
+++ src/main/java/fhv/team11/project/ems/commons/address/Address.java
@@ -2,16 +2,26 @@ package fhv.team11.project.ems.commons.address;
 
 import fhv.team11.project.ems.events.repo.Blueprint;
 import fhv.team11.project.ems.events.repo.Category;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Address {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(mappedBy = "address")
     private Blueprint blueprint;
+
+    private String country;
+    private String region;
+    private String city;
+    private String zip;
+    private String street;
+    private String number;
+    private String optionalText;
 }
