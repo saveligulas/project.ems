43653c3284741bebc5db3d5168c996cac8730385
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-09 03:20:53 +0100

    added isAuthenticated and @Data

--- src/main/java/fhv/team11/project/ems/commons/home/base/attribute/GlobalAttributes.java
+++ src/main/java/fhv/team11/project/ems/commons/home/base/attribute/GlobalAttributes.java
@@ -1,5 +1,8 @@
 package fhv.team11.project.ems.commons.home.base.attribute;
 
+import lombok.Data;
+
+@Data
 public class GlobalAttributes {
     private final EventAttributes event;
     private final UserViewModel user;
@@ -7,5 +10,9 @@ public class GlobalAttributes {
     public GlobalAttributes() {
         this.event = new EventAttributes();
         this.user = new UserViewModel();
+    }
+
+    public boolean isAuthenticated() {
+        return user.getUsername() != null;
     }
 }
