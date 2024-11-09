9fc20797d964dd295fe6f28e3589b6cd2b97b759
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-09 03:28:40 +0100

    added Validation

--- src/main/java/fhv/team11/project/ems/user/repo/entity/UserJDBC.java
+++ src/main/java/fhv/team11/project/ems/user/repo/entity/UserJDBC.java
@@ -1,6 +1,10 @@
 package fhv.team11.project.ems.user.repo.entity;
 
 import fhv.team11.project.ems.user.repo.Role;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,10 +19,18 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class UserJDBC implements UserDetails {
 
+    @NotNull
+    @Min(0)
     private Long id;
+
+    @NotBlank
     private String email;
+
     @Getter(AccessLevel.NONE)
+    @NotBlank
     private String password;
+
+    @NotEmpty
     private List<Role> roles;
 
     @Override
