6fc992d279227303f7b0674634354c170c9be498
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-09 03:28:04 +0100

    added empty constructor

--- src/main/java/fhv/team11/project/ems/security/error/RegistrationError.java
+++ src/main/java/fhv/team11/project/ems/security/error/RegistrationError.java
@@ -4,4 +4,7 @@ public class RegistrationError extends RuntimeException {
     public RegistrationError(String message) {
         super(message);
     }
+    public RegistrationError() {
+        super("Registration failed. Please make sure to fill in all required fields and try again.");
+    }
 }
