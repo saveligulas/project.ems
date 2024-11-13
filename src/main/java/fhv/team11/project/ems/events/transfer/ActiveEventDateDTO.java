072a2b7e1e49f6aa4766db5f2d0fee4371abc827
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-13 18:27:08 +0100

    dto for event dates

---
+++ src/main/java/fhv/team11/project/ems/events/transfer/ActiveEventDateDTO.java
@@ -0,0 +1,31 @@
+package fhv.team11.project.ems.events.transfer;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Data
+public class ActiveEventDateDTO implements Comparable<ActiveEventDateDTO> {
+    private Date date;
+    private String name;
+
+    @Override
+    public int compareTo(ActiveEventDateDTO o) {
+        return this.date.compareTo(o.date);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ActiveEventDateDTO that = (ActiveEventDateDTO) o;
+        return Objects.equals(date, that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(date);
+    }
+}
