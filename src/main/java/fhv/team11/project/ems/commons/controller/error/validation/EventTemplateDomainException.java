1b2aa9f02bb7c8d396f222bffc16949bd0d70e60
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-14 01:38:29 +0100

    added new exception

---
+++ src/main/java/fhv/team11/project/ems/commons/controller/error/validation/EventTemplateDomainException.java
@@ -0,0 +1,7 @@
+package fhv.team11.project.ems.commons.controller.error.validation;
+
+public class EventTemplateDomainException extends RuntimeException {
+    public EventTemplateDomainException(String message) {
+        super(message);
+    }
+}
