e75567babba7052a2cf39ef2b568c2c1377263f5
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-14 01:41:51 +0100

    added IDomainValidator

---
+++ src/main/java/fhv/team11/project/ems/commons/validation/domain/IDomainValidator.java
@@ -0,0 +1,26 @@
+package fhv.team11.project.ems.commons.validation.domain;
+
+import fhv.team11.project.ems.commons.model.IModelAttributeName;
+import jakarta.validation.ConstraintViolation;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+
+import java.util.Set;
+
+public interface IDomainValidator<DTO extends IModelAttributeName> {
+    BindingResult validate(DTO dto);
+
+    default BindingResult buildBindingResult(DTO dto) {
+        return new BeanPropertyBindingResult(dto, dto.getModelAttributeName());
+    }
+
+    @Deprecated
+    default void addToBindingResult(Set<ConstraintViolation<?>> violations, BindingResult bindingResult) {
+        violations.forEach(violation -> {
+            String property = violation.getPropertyPath().toString();
+            bindingResult.rejectValue(property,
+                    "invalid." + property,
+                    violation.getMessage());
+        });
+    }
+}
