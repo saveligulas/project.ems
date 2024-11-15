db2e4e51f32300b90912d06e727e82515d9dcc25
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-15 02:05:36 +0100

    import package change in AuthenticationRequestValidationException

--- src/main/java/fhv/team11/project/ems/security/transfer/domain/error/AuthenticationRequestValidationException.java
+++ src/main/java/fhv/team11/project/ems/security/transfer/domain/error/AuthenticationRequestValidationException.java
@@ -1,4 +1,4 @@
-package fhv.team11.project.ems.security.jwt.domain.error;
+package fhv.team11.project.ems.security.transfer.domain.error;
 
 public class AuthenticationRequestValidationException extends RuntimeException {
     public AuthenticationRequestValidationException(String message) {
