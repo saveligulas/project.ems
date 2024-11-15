4ffe61266216912b73fff750db59e4692329e645
DanielJussel <daniel.jussel@students.fhv.at>
2024-11-15 22:54:11 +0100

    For TODO Schedule

---
+++ src/main/java/fhv/team11/project/ems/events/controller/ScheduleEventController.java
@@ -0,0 +1,8 @@
+package fhv.team11.project.ems.events.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ScheduleEventController {
+    // TODO: EVERYTHING
+}
