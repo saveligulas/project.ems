763fa07a089684ab3dd180590fc8224359694c00
DanielJussel <daniel.jussel@students.fhv.at>
2024-11-16 18:05:27 +0100

    ScheduleEventDTO

--- src/main/java/fhv/team11/project/ems/events/transfer/ScheduleEventDTO.java
+++ src/main/java/fhv/team11/project/ems/events/transfer/ScheduleEventDTO.java
@@ -3,8 +3,11 @@ package fhv.team11.project.ems.events.transfer;
 
 import lombok.Data;
 
+import java.time.LocalTime;
+
 @Data
 public class ScheduleEventDTO {
-    // TODO: EVERYTHING
+    private LocalTime startTime;
+    private LocalTime endTime;
 
 }
