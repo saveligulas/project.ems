966523412b02c0b845f75480a60da6d59b6baf15
DanielJussel <daniel.jussel@students.fhv.at>
2024-11-19 22:20:36 +0100

    Creat event wizard service

--- src/main/java/fhv/team11/project/ems/events/service/ActiveEventWizardService.java
+++ src/main/java/fhv/team11/project/ems/events/service/ActiveEventWizardService.java
@@ -1,19 +1,54 @@
 package fhv.team11.project.ems.events.service;
 
-import fhv.team11.project.ems.events.error.DuplicateActiveEventDateException;
+import fhv.team11.project.ems.events.repo.*;
 import fhv.team11.project.ems.events.transfer.ActiveEventDateDTO;
 import fhv.team11.project.ems.events.transfer.ActiveEventWizardDTO;
+import fhv.team11.project.ems.events.transfer.EventTemplateDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.BindingResult;
 
 @Service
+
 public class ActiveEventWizardService {
 
-    public void addDateToWizard(ActiveEventWizardDTO wizardDTO, ActiveEventDateDTO dateDTO) {
-        if (!wizardDTO.getActiveEventDates().add(dateDTO)) {
+    private final ActiveEventRepository activeEventRepository;
+    private final AppointmentRepository appointmentRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final EventDateRepository eventDateRepository;
+    private final EventTemplateRepository eventTemplateRepository;
 
+    @Autowired
+    public ActiveEventWizardService(ActiveEventRepository activeEventRepository,
+                                    AppointmentRepository appointmentRepository,
+                                    ScheduleRepository scheduleRepository,
+                                    EventDateRepository eventDateRepository,
+                                    EventTemplateRepository eventTemplateRepository) {
+        this.activeEventRepository = activeEventRepository;
+        this.appointmentRepository = appointmentRepository;
+        this.scheduleRepository = scheduleRepository;
+        this.eventDateRepository = eventDateRepository;
+        this.eventTemplateRepository = eventTemplateRepository;
+    }
+
+    @Transactional
+    public void createActiveEvent(ActiveEventWizardDTO activeEventWizardDTO) {
+
+        ActiveEvent activeEvent = new ActiveEvent();
+        activeEvent.setEventTemplate(eventTemplateRepository.findById(activeEventWizardDTO.getTemplateId()).orElse(null));
+        activeEventRepository.save(activeEvent);
+        Schedule schedule = new Schedule();
+        scheduleRepository.save(schedule);
+
+        for(ActiveEventDateDTO AED: activeEventWizardDTO.getActiveEventDates()){
+            EventDate eventDate = EventDateDTOMapper.INSTANCE.getEntity(AED);
+            eventDate.setActiveEvent(activeEvent);
+            eventDate.setSchedule(schedule);
+            eventDateRepository.save(eventDate);
         }
+        Appointment appointment = AppointmentDTOMapper.INSTANCE.getEntity(activeEventWizardDTO);
+        appointment.setSchedule(schedule);
+        appointmentRepository.save(appointment);
 
     }
 }
