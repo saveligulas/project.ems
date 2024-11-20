aee9d2a8bf13f39a045b50dd6c48e8b3d27815c6
Daniel <danielshahini@outlook.de>
2024-11-20 17:07:42 +0100

    Security geändert sodass es nur in eine session bleibt

--- src/main/java/fhv/team11/project/ems/security/config/SecurityConfig.java
+++ src/main/java/fhv/team11/project/ems/security/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.NullSecurityContextRepository;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
@@ -40,7 +41,7 @@ public class SecurityConfig {
                 session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
         );
         http.securityContext(context -> context
-                .securityContextRepository(new NullSecurityContextRepository())
+                .securityContextRepository(new HttpSessionSecurityContextRepository())
         );
         http.userDetailsService(userDetailsService);
         http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
