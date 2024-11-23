60ab3c5eedf4fc3da4691ef7cc04cb229e2c5f6b
saveligulas <saveli.gulas@gmail.com>
2024-11-23 18:07:34 +0100

    added Profile fields

--- src/main/java/fhv/team11/project/ems/user/repo/entity/User.java
+++ src/main/java/fhv/team11/project/ems/user/repo/entity/User.java
@@ -2,10 +2,13 @@ package fhv.team11.project.ems.user.repo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Optional;
 
 @Entity
 @Table(name = "user")
@@ -15,4 +18,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     private Long id;
+
+    @OneToOne(mappedBy = "user")
+    private Customer customerProfile;
 }
