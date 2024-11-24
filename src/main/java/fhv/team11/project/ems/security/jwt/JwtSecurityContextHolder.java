bebff5f66515bd06fb19d803079a89aa0262cc74
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-24 11:51:19 +0100

    renamed User

--- src/main/java/fhv/team11/project/ems/security/jwt/JwtSecurityContextHolder.java
+++ src/main/java/fhv/team11/project/ems/security/jwt/JwtSecurityContextHolder.java
@@ -1,21 +1,21 @@
 package fhv.team11.project.ems.security.jwt;
 
 import fhv.team11.project.ems.security.error.SecuredEndpointAccessException;
-import fhv.team11.project.ems.user.repo.entity.User;
-import fhv.team11.project.ems.user.repo.entity.UserJDBC;
+import fhv.team11.project.ems.user.entity.UserEntity;
+import fhv.team11.project.ems.user.entity.UserJDBC;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class JwtSecurityContextHolder {
 
     //TODO: add method to get User Domain Model with different Profiles
-    public static User getUser() {
+    public static UserEntity getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null) {
             Object principal = authentication.getPrincipal();
             if (principal instanceof UserJDBC) {
                 //TODO: change this to fetch user from repository to get customerProfile
-                return new User(((UserJDBC) principal).getId(), null);
+                return new UserEntity(((UserJDBC) principal).getId());
             }
         }
         throw new SecuredEndpointAccessException();
--- src/main/java/fhv/team11/project/ems/user/repo/UserRepositoryQuery.java
+++ src/main/java/fhv/team11/project/ems/user/repo/UserRepositoryQuery.java
@@ -1,7 +1,7 @@
 package fhv.team11.project.ems.user.repo;
 
 import fhv.team11.project.ems.commons.database.IRepository;
-import fhv.team11.project.ems.user.repo.entity.UserJDBC;
+import fhv.team11.project.ems.user.entity.UserJDBC;
 import jakarta.persistence.PersistenceException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -105,5 +105,15 @@ public class UserRepositoryQuery implements IRepository<UserJDBC, Long> {
         SqlParameterSource params = new MapSqlParameterSource("id", id);
 
         namedParameterJdbcTemplate.update(sql, params);
+    }
+
+    public List<UserJDBC> findUsersWithPagination(int offset, int pageSize) {
+        String sql = "SELECT * FROM public.user ORDER BY id LIMIT :limit OFFSET :offset";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("limit", pageSize);
+        params.addValue("offset", offset);
+
+        return namedParameterJdbcTemplate.query(sql, params, new UserRowMapper());
     }
 }
