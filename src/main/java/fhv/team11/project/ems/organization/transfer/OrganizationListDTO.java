fef50874509cb577258e4df6e8d307c616d565c6
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-24 11:50:04 +0100

    added DTOs for profiles and organizations

---
+++ src/main/java/fhv/team11/project/ems/organization/transfer/OrganizationListDTO.java
@@ -0,0 +1,8 @@
+package fhv.team11.project.ems.organization.transfer;
+
+public class OrganizationListDTO {
+    private Long id;
+    private OrganizationType type;
+    private String name;
+    private String shortName;
+}
---
+++ src/main/java/fhv/team11/project/ems/user/transfer/CustomerProfileDTO.java
@@ -0,0 +1,10 @@
+package fhv.team11.project.ems.user.transfer;
+
+import fhv.team11.project.ems.commons.address.AddressDTO;
+
+public class CustomerProfileDTO {
+    private String firstName;
+    private String lastName;
+    private AddressDTO addressDTO;
+    private String phoneNumber;
+}
