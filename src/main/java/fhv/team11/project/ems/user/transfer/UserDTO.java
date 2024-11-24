9206f2c5f1cfab50578cccc5310a7ef5cc3e260a
Saveli Gulas <saveli.gulas@gmail.com>
2024-11-24 11:53:51 +0100

    filled UserDTO

--- src/main/java/fhv/team11/project/ems/user/transfer/UserDTO.java
+++ src/main/java/fhv/team11/project/ems/user/transfer/UserDTO.java
@@ -1,4 +1,16 @@
 package fhv.team11.project.ems.user.transfer;
 
+import fhv.team11.project.ems.user.profile.AdministratorProfile;
+import fhv.team11.project.ems.user.profile.EventOrganizerProfile;
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
 public class UserDTO {
+    private String username;
+    private Optional<CustomerProfileDTO> customerProfileDTO;
+    private Optional<EventOrganizerProfileDTO> eventOrganizerProfile;
+    private Optional<AdministratorProfileDTO> administratorProfile;
+    private Optional<BackOfficeEmployeeProfileDTO> backOfficeEmployeeProfileDTO;
 }
