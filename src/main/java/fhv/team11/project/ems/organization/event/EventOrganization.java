a140d5a34c768d48ec431f0932bb2003a31c6710
saveligulas <saveli.gulas@gmail.com>
2024-11-25 11:26:24 +0100

    persisting and generation for fields in EventOrganization

--- src/main/java/fhv/team11/project/ems/organization/event/EventOrganization.java
+++ src/main/java/fhv/team11/project/ems/organization/event/EventOrganization.java
@@ -1,18 +1,26 @@
 package fhv.team11.project.ems.organization.event;
 
 import fhv.team11.project.ems.organization.PartnerOrganization;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class EventOrganization {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "organization_id")
     private PartnerOrganization partnerOrganization;
 }
