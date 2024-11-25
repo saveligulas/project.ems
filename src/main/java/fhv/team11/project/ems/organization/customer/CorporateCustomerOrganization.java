1a04d8768c0846c49267c51505d4fbc28e2c7e7e
saveligulas <saveli.gulas@gmail.com>
2024-11-25 11:25:15 +0100

    added persist cascading

--- src/main/java/fhv/team11/project/ems/organization/customer/CorporateCustomerOrganization.java
+++ src/main/java/fhv/team11/project/ems/organization/customer/CorporateCustomerOrganization.java
@@ -2,15 +2,19 @@ package fhv.team11.project.ems.organization.customer;
 
 import fhv.team11.project.ems.organization.PartnerOrganization;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class CorporateCustomerOrganization {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "organization_id")
     private PartnerOrganization partnerOrganization;
 }
