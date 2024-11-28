d1e0c8d6365749b62f8f92f9ac878036c4d0defd
saveligulas <saveli.gulas@gmail.com>
2024-11-28 13:14:44 +0100

    added another implementation for HandleBindingResultException.addBindingResultToRedirect

--- src/main/java/fhv/team11/project/ems/commons/validation/domain/handler/HandleBindingResultException.java
+++ src/main/java/fhv/team11/project/ems/commons/validation/domain/handler/HandleBindingResultException.java
@@ -3,6 +3,7 @@ package fhv.team11.project.ems.commons.validation.domain.handler;
 import fhv.team11.project.ems.commons.controller.error.handler.HandleRedirectException;
 import fhv.team11.project.ems.commons.validation.domain.error.BindingResultException;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 public interface HandleBindingResultException extends HandleRedirectException {
@@ -11,8 +12,12 @@ public interface HandleBindingResultException extends HandleRedirectException {
     }
 
     default void addBindingResultToRedirect(BindingResultException ex, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute(ex.getModelAttributeName(), ex.getBindingResult().getTarget());
-        redirectAttributes.addFlashAttribute(getBindingResultKey(ex.getModelAttributeName()), ex.getBindingResult());
+        addBindingResultToRedirect(ex.getModelAttributeName(), ex.getBindingResult(), redirectAttributes);
+    }
+
+    default void addBindingResultToRedirect(String modelAttributeName, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(modelAttributeName, bindingResult.getTarget());
+        redirectAttributes.addFlashAttribute(getBindingResultKey(modelAttributeName), bindingResult);
     }
 
     default void addBindingResultToModel(BindingResultException ex, Model model) {
