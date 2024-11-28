03d1f72ff480599da7c627604e939de6e360eff0
saveligulas <saveli.gulas@gmail.com>
2024-11-28 13:18:51 +0100

    changed JwtSecurityContextHolder to be a bean to use UserEntityRepository

--- src/main/java/fhv/team11/project/ems/security/jwt/JwtSecurityContextHolder.java
+++ src/main/java/fhv/team11/project/ems/security/jwt/JwtSecurityContextHolder.java
@@ -2,11 +2,22 @@ package fhv.team11.project.ems.security.jwt;
 
 import fhv.team11.project.ems.security.error.SecuredEndpointAccessException;
 import fhv.team11.project.ems.user.entity.UserEntity;
+import fhv.team11.project.ems.user.entity.UserEntityRepository;
 import fhv.team11.project.ems.user.entity.UserJDBC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtSecurityContextHolder {
+
+    private static UserEntityRepository userEntityRepository;
+
+    @Autowired
+    public JwtSecurityContextHolder(UserEntityRepository userEntityRepository) {
+        JwtSecurityContextHolder.userEntityRepository = userEntityRepository;
+    }
 
     //TODO: add method to get User Domain Model with different Profiles
     public static UserEntity getUser() {
@@ -14,8 +25,8 @@ public class JwtSecurityContextHolder {
         if (authentication != null) {
             Object principal = authentication.getPrincipal();
             if (principal instanceof UserJDBC) {
-                //TODO: change this to fetch user from repository to get customerProfile
-                return new UserEntity(((UserJDBC) principal).getId());
+                Long id = ((UserJDBC) principal).getId();
+                return userEntityRepository.findById(id).orElseThrow(SecuredEndpointAccessException::new);
             }
         }
         throw new SecuredEndpointAccessException();
