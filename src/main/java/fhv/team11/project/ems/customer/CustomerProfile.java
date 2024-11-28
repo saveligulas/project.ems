8bb80fd0ae3b2b7b388b5a071fc669831e2c5a40
saveligulas <saveli.gulas@gmail.com>
2024-11-28 13:05:10 +0100

    moved profiles to respective packages

--- src/main/java/fhv/team11/project/ems/customer/CustomerProfile.java
+++ src/main/java/fhv/team11/project/ems/customer/CustomerProfile.java
@@ -1,15 +1,15 @@
-package fhv.team11.project.ems.user.profile;
+package fhv.team11.project.ems.customer;
 
 import fhv.team11.project.ems.commons.address.Address;
+import fhv.team11.project.ems.user.profile.UserProfile;
 import jakarta.persistence.*;
 
 @Entity
-public class CustomerProfile extends UserProfile{
+public class CustomerProfile extends UserProfile {
 
     private String firstName;
     private String lastName;
 
-    //TODO: ManyToMany
     @OneToOne
     @JoinColumn(name = "address_id")
     private Address address;
--- src/main/java/fhv/team11/project/ems/organization/EventOrganizerProfile.java
+++ src/main/java/fhv/team11/project/ems/organization/EventOrganizerProfile.java
@@ -1,5 +1,6 @@
-package fhv.team11.project.ems.user.profile;
+package fhv.team11.project.ems.organization;
 
+import fhv.team11.project.ems.user.profile.UserProfile;
 import jakarta.persistence.Entity;
 
 @Entity
