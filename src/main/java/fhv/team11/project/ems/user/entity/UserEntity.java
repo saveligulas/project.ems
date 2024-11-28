a15f139755a1e0ae894b94d128543ab03ffbcb0d
saveligulas <saveli.gulas@gmail.com>
2024-11-28 13:19:57 +0100

    added mapping to UserEntityDetails

--- src/main/java/fhv/team11/project/ems/user/entity/UserEntity.java
+++ src/main/java/fhv/team11/project/ems/user/entity/UserEntity.java
@@ -2,6 +2,7 @@ package fhv.team11.project.ems.user.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,4 +16,7 @@ import lombok.NoArgsConstructor;
 public class UserEntity {
     @Id
     private Long id;
+
+    @OneToOne(mappedBy = "user")
+    private UserEntityDetails userEntityDetails;
 }
