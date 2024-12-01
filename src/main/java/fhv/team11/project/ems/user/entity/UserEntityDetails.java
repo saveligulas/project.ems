508352d45e7db8edac2ded1a93b407ba2777ed27
Saveli Gulas <saveli.gulas@gmail.com>
2024-12-02 00:21:33 +0100

    updated UserEntityDetails

--- src/main/java/fhv/team11/project/ems/user/entity/UserEntityDetails.java
+++ src/main/java/fhv/team11/project/ems/user/entity/UserEntityDetails.java
@@ -2,6 +2,7 @@ package fhv.team11.project.ems.user.entity;
 
 import fhv.team11.project.ems.admin.AdministratorProfile;
 import fhv.team11.project.ems.customer.CustomerProfile;
+import fhv.team11.project.ems.organization.EventOrganizerProfile;
 import fhv.team11.project.ems.user.profile.BackOfficeProfile;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -11,28 +12,52 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 @Entity
-@Getter
 @Setter
 public class UserEntityDetails {
 
     @Id
     @GeneratedValue
+    @Getter
     private Long id;
 
     @OneToOne
     @JoinColumn(name = "user_id")
+    @Getter
     private UserEntity user;
 
+    @Nullable
     @OneToOne(mappedBy = "userEntityDetails")
     private CustomerProfile customerProfile;
 
+    @Nullable
     @OneToOne(mappedBy = "userEntityDetails")
     private BackOfficeProfile backOfficeProfile;
 
+    @Nullable
     @OneToOne(mappedBy = "userEntityDetails")
     private AdministratorProfile administratorProfile;
 
-    //TODO:
+    @Nullable
+    @OneToOne(mappedBy = "userEntityDetails")
+    private EventOrganizerProfile eventOrganizerProfile;
+
+    public @Nullable CustomerProfile getCustomerProfile() {
+        return customerProfile;
+    }
+
+    public @Nullable BackOfficeProfile getBackOfficeProfile() {
+        return backOfficeProfile;
+    }
+
+    public @Nullable AdministratorProfile getAdministratorProfile() {
+        return administratorProfile;
+    }
+
+    public @Nullable EventOrganizerProfile getEventOrganizerProfile() {
+        return eventOrganizerProfile;
+    }
 }
