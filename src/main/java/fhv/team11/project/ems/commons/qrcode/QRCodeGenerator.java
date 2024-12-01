1736839cf51502be7f060fb60277d843cc476e99
DanielJussel <daniel.jussel@students.fhv.at>
2024-12-01 23:35:45 +0100

    Class for generating individual QRCodes with token and url

---
+++ src/main/java/fhv/team11/project/ems/commons/qrcode/QRCodeGenerator.java
@@ -0,0 +1,18 @@
+package fhv.team11.project.ems.commons.qrcode;
+import net.glxn.qrgen.javase.QRCode;
+
+import java.util.Base64;
+
+public class QRCodeGenerator {
+
+    public static String generateQRCodeImage(String URL, String Token, int width, int height) {
+        String QRCodeURL = URL + "/?token=" + Token;
+        String qrCodeBase64 = Base64.getEncoder().encodeToString(QRCode
+                                                                .from(QRCodeURL)
+                                                                .withSize(width,height)
+                                                                .stream()
+                                                                .toByteArray());
+
+        return qrCodeBase64;
+    }
+}
