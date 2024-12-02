55898f24cc6a4c94b54406f3306d16a646a7eeca
saveligulas <saveli.gulas@gmail.com>
2024-12-02 20:01:58 +0100

    UserAttributes udated

--- src/main/java/fhv/team11/project/ems/commons/controller/base/attribute/user/UserAttributeDTO.java
+++ src/main/java/fhv/team11/project/ems/commons/controller/base/attribute/user/UserAttributeDTO.java
@@ -1,11 +1,13 @@
 package fhv.team11.project.ems.commons.controller.base.attribute.user;
 
+import fhv.team11.project.ems.backoffice.transfer.BackOfficeProfileDTOMapper;
 import fhv.team11.project.ems.customer.transfer.CustomerProfileDTO;
 import fhv.team11.project.ems.customer.transfer.CustomerProfileDTOMapper;
 import fhv.team11.project.ems.security.error.SecuredEndpointAccessException;
 import fhv.team11.project.ems.security.jwt.JwtSecurityContextHolder;
 import fhv.team11.project.ems.user.entity.UserEntity;
 import fhv.team11.project.ems.user.entity.UserJDBC;
+import fhv.team11.project.ems.backoffice.transfer.BackOfficeProfileDTO;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -18,10 +20,14 @@ public class UserAttributeDTO {
     final String username;
     @Nullable
     final CustomerProfileDTO customerProfileDTO;
+    @Nullable
+    final BackOfficeProfileDTO backOfficeProfileDTO;
 
     public UserAttributeDTO() {
         String name = null;
         CustomerProfileDTO customerProfile = null;
+        BackOfficeProfileDTO backOfficeProfile = null;
+
         try {
             UserEntity user = JwtSecurityContextHolder.getUser();
             UserJDBC userJDBC = JwtSecurityContextHolder.getUserJDBC();
@@ -32,11 +38,14 @@ public class UserAttributeDTO {
                 log.info("User has not set their UserEntityDetails");
             } else {
                 customerProfile = CustomerProfileDTOMapper.INSTANCE.getDTO(user.getUserEntityDetails().getCustomerProfile());
+                backOfficeProfile = BackOfficeProfileDTOMapper.INSTANCE.getDTO(user.getUserEntityDetails().getBackOfficeProfile());
             }
         } catch (SecuredEndpointAccessException e) {
             log.info("User is not logged in");
         }
+
         this.username = name;
         this.customerProfileDTO = customerProfile;
+        this.backOfficeProfileDTO = backOfficeProfile;
     }
 }
--- src/main/java/fhv/team11/project/ems/commons/controller/base/attribute/user/UserAttributes.java
+++ src/main/java/fhv/team11/project/ems/commons/controller/base/attribute/user/UserAttributes.java
@@ -1,10 +1,11 @@
 package fhv.team11.project.ems.commons.controller.base.attribute.user;
 
+import fhv.team11.project.ems.backoffice.transfer.BackOfficeProfileDTO;
 import fhv.team11.project.ems.customer.transfer.CustomerProfileDTO;
+import fhv.team11.project.ems.security.error.SecuredEndpointAccessException;
+import fhv.team11.project.ems.security.jwt.JwtSecurityContextHolder;
 import fhv.team11.project.ems.security.permission.role.Role;
-import jakarta.validation.constraints.Null;
 import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,15 +16,33 @@ public class UserAttributes {
     private static final List<Role> ROLES = Arrays.asList(Role.values());
 
     private final UserAttributeDTO attributes;
+    private final List<Role> roles;
     private final List<String> permissions;
 
     public UserAttributes() {
         this.attributes = new UserAttributeDTO();
+
+        List<Role> roles = new ArrayList<>();
+        try {
+            roles = JwtSecurityContextHolder.getUserJDBC().getRoles().stream().toList();
+        } catch (SecuredEndpointAccessException ignored) {
+
+        }
+        this.roles = roles;
+
         this.permissions = new ArrayList<>();
     }
 
     public boolean isAuthenticated() {
         return attributes.username != null;
+    }
+
+    public boolean hasRole(Role role) {
+        return roles.contains(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        return hasRole(Role.valueOf(roleName));
     }
 
     public boolean hasPermission(String permission) {
@@ -46,5 +65,16 @@ public class UserAttributes {
             throw new IllegalStateException("Customer profile not set");
         }
         return attributes.customerProfileDTO;
+    }
+
+    public boolean hasBackOfficeProfile() {
+        return attributes.backOfficeProfileDTO != null;
+    }
+
+    public BackOfficeProfileDTO getBackOfficeProfileDTO() {
+        if (attributes.backOfficeProfileDTO == null) {
+            throw new IllegalStateException("BackOffice profile not set");
+        }
+        return attributes.backOfficeProfileDTO;
     }
 }
