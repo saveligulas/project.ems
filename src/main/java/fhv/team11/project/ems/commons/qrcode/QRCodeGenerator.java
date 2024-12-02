cea68032aca39d704a1600db4d975b95a3e222a0
DanielJussel <daniel.jussel@students.fhv.at>
2024-12-03 00:25:01 +0100

    Url fix

--- src/main/java/fhv/team11/project/ems/commons/qrcode/QRCodeGenerator.java
+++ src/main/java/fhv/team11/project/ems/commons/qrcode/QRCodeGenerator.java
@@ -6,7 +6,7 @@ import java.util.Base64;
 public class QRCodeGenerator {
 
     public static String generateQRCodeImage(String URL, String Token, int width, int height) {
-        String QRCodeURL = URL + "/?token=" + Token;
+        String QRCodeURL = URL + "?token=" + Token;
         String qrCodeBase64 = Base64.getEncoder().encodeToString(QRCode
                                                                 .from(QRCodeURL)
                                                                 .withSize(width,height)
