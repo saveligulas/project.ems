6a1a55a71105fb57d96664715a97407fe5c6268a
Saveli Gulas <saveli.gulas@gmail.com>
2024-12-06 21:53:34 +0100

    import and naming changes

--- src/main/java/fhv/team11/project/ems/user/transfer/UserDTO.java
+++ src/main/java/fhv/team11/project/ems/user/transfer/UserDTO.java
@@ -1,13 +1,14 @@
 package fhv.team11.project.ems.user.transfer;
 
 import fhv.team11.project.ems.backoffice.transfer.BackOfficeProfileDTO;
+import fhv.team11.project.ems.commons.validation.model.IModelAttribute;
 import fhv.team11.project.ems.customer.transfer.CustomerProfileDTO;
 import lombok.Data;
 
 import java.util.Optional;
 
 @Data
-public class UserDTO {
+public class UserDTO implements IModelAttribute {
 
     private String username;
     private Optional<CustomerProfileDTO> customerProfileDTO;
