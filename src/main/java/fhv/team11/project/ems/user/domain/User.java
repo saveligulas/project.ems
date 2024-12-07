7d60262ef7e9fe61a90746bacf3d16f83de5076f
saveligulas <saveli.gulas@gmail.com>
2024-12-07 21:58:22 +0100

    Domain Infrastructure

---
+++ src/main/java/fhv/team11/project/ems/user/domain/User.java
@@ -0,0 +1,4 @@
+package fhv.team11.project.ems.user.domain;
+
+public class User {
+}
---
+++ src/main/java/fhv/team11/project/ems/user/domain/UserDomainFactory.java
@@ -0,0 +1,32 @@
+package fhv.team11.project.ems.user.domain;
+
+import fhv.team11.project.ems.commons.domain.DomainFactory;
+import fhv.team11.project.ems.commons.validation.domain.DomainValidatorFactory;
+import fhv.team11.project.ems.user.entity.UserEntity;
+import fhv.team11.project.ems.user.transfer.UserView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDomainFactory extends DomainFactory<UserEntity, User, UserView> {
+
+    @Autowired
+    public UserDomainFactory(DomainValidatorFactory domainValidatorFactory) {
+        super(domainValidatorFactory);
+    }
+
+    @Override
+    public User getDomain() {
+        return null;
+    }
+
+    @Override
+    public User getDomainFromEntity(UserEntity entity) {
+        return null;
+    }
+
+    @Override
+    public User getDomainFromModel(UserView model) {
+        return null;
+    }
+}
