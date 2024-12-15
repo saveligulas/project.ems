c7385400bf5c31673a2930dfc05cde5b4daf5c3b
Saveli Gulas <saveli.gulas@gmail.com>
2024-12-16 00:23:44 +0100

    removed unused Domain classes

--- src/main/java/fhv/team11/project/ems/commons/domain/DomainConstraintException.java
+++
@@ -1,7 +0,0 @@
-package fhv.team11.project.ems.commons.domain;
-
-public class DomainConstraintException extends DomainValidationException {
-    public DomainConstraintException(String message, String field) {
-        super(message, field);
-    }
-}
