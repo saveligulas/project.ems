2d847eb38a5a8b16fd24c9afc5043ec10811fe4b
Saveli Gulas <saveli.gulas@gmail.com>
2024-12-15 23:37:18 +0100

    fixed EventTemplateDTOValidationException

--- src/main/java/fhv/team11/project/ems/events/error/EventTemplateDTOValidationException.java
+++ src/main/java/fhv/team11/project/ems/events/error/EventTemplateDTOValidationException.java
@@ -3,16 +3,9 @@ package fhv.team11.project.ems.events.error;
 import fhv.team11.project.ems.commons.validation.domain.error.BindingResultException;
 import org.springframework.validation.BindingResult;
 
-public class EventTemplateDTOValidationException extends RuntimeException {
-    private final BindingResult bindingResult;
-
+public class EventTemplateDTOValidationException extends BindingResultException {
     public EventTemplateDTOValidationException(BindingResult bindingResult) {
-        super("Validation failed");
-        this.bindingResult = bindingResult;
-    }
-
-    public BindingResult getBindingResult() {
-        return bindingResult;
+        super(bindingResult, "eventTemplate", "event/manage");
     }
 }
 
