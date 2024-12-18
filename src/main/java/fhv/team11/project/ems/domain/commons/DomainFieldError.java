f0c9da18c40e0d362b0f5657ab08971aafd1d9da
Saveli Gulas <saveli.gulas@gmail.com>
2024-12-18 23:04:08 +0100

    added and refactored Domain Errors for collecting in constructor

--- src/main/java/fhv/team11/project/ems/domain/commons/DomainFieldError.java
+++ src/main/java/fhv/team11/project/ems/domain/commons/DomainFieldError.java
@@ -1,27 +1,11 @@
 package fhv.team11.project.ems.domain.commons;
 
-import lombok.Getter;
-import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
-
-import java.util.Collections;
-import java.util.List;
-
-@NullMarked
-@Getter
 public class DomainFieldError {
-    private final String fieldName;
-    private final String message;
-    private final List<String> responsibleFieldNames;
+    final String fieldName;
+    final String errorMessage;
 
-    public DomainFieldError(String fieldName, String message, @Nullable List<String> responsibleFieldNames) {
+    public DomainFieldError(String fieldName, String errorMessage) {
         this.fieldName = fieldName;
-        this.message = message;
-
-        if (responsibleFieldNames == null) {
-            this.responsibleFieldNames = Collections.EMPTY_LIST;
-        } else {
-            this.responsibleFieldNames = Collections.unmodifiableList(responsibleFieldNames);
-        }
+        this.errorMessage = errorMessage;
     }
 }
