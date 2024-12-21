ca48bc3a3077d4b677aad4a4f96a3f1abafff47b
DanielJussel <daniel.jussel@students.fhv.at>
2024-12-21 18:13:54 +0100

    Changed template model to new structur

--- src/main/java/fhv/team11/project/ems/domain/events/TemplateModel.java
+++ src/main/java/fhv/team11/project/ems/domain/events/TemplateModel.java
@@ -1,24 +1,22 @@
 package fhv.team11.project.ems.domain.events;
 
-import fhv.team11.project.ems.commons.domain.DomainFieldCollectionValidationException;
-import fhv.team11.project.ems.commons.domain.DomainFieldValidationException;
-import fhv.team11.project.ems.commons.domain.DomainInstantiationException;
-import fhv.team11.project.ems.commons.domain.IDomainObject;
+
 import fhv.team11.project.ems.domain.adress.AddressModel;
 
 import fhv.team11.project.ems.domain.commons.*;
+import fhv.team11.project.ems.domain.commons.exception.DomainFieldValidationException;
 import fhv.team11.project.ems.events.repo.EventCategory;
 
+import lombok.Getter;
 import lombok.Setter;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
+@Getter
 public class TemplateModel implements IDomainObject {
-    @Nullable
-    private transient HashMap<String, String> fieldErrors;
+    private final DomainObjectConstructorHelper constructorHelper;
 
     @Nullable
     private Long id;
@@ -29,72 +27,79 @@ public class TemplateModel implements IDomainObject {
     private double price;
     private Integer maxParticipants;
     private Integer minParticipants;
+    private Integer overbookedPlaces;
     @Setter
     private AddressModel address;
 
-    public TemplateModel(Long id, String name, EventCategory category, double price, int maxParticipants, int minParticipants, AddressModel address) throws DomainFieldCollectionValidationException, DomainFieldValidationException {
-        fieldErrors = new HashMap<>();
+    public TemplateModel(Long id, String name, EventCategory category, double price, int maxParticipants, int minParticipants, AddressModel address, Integer overbookedPlaces) throws DomainFieldValidationException {
+        this.constructorHelper = new DomainObjectConstructorHelper();
+
 
         setId(id);
         setName(name);
         setCategory(category);
         setPrice(price);
+        setOverbookedPlaces(overbookedPlaces);
         setParticipants(maxParticipants, minParticipants);
         setAddress(address);
+
+        this.constructorHelper.finish();
+    }
+
+    public void setOverbookedPlaces(Integer overbookedPlaces) throws DomainFieldValidationException {
+
+        String fieldName;
+        String errorMessage;
+
+        if (!NameValidator.isValid(name)) {
+            fieldName = "OverBookedPlaces";
+            errorMessage = "OverBookedPLaces can not be negative";
+
+            handleError(fieldName,errorMessage,constructorHelper);
+        }
+
+        this.overbookedPlaces = overbookedPlaces;
     }
 
     private void setName(String name) throws DomainFieldValidationException {
+        String fieldName;
+        String errorMessage;
 
         if (!NameValidator.isValid(name)) {
-            String fieldName = "Name";
-            String errorMessage = "";
+            fieldName = "Name";
+            errorMessage = "";
 
-            if (this.isInstantiated()) {
-                throw new DomainFieldValidationException(fieldName, errorMessage);
-            } else {
-                fieldErrors.put(fieldName, errorMessage);
-            }
+            handleError(fieldName,errorMessage,constructorHelper);
         }
 
     }
 
-    public void setParticipants(int maxParticipants, int minParticipants) throws DomainFieldValidationException, DomainFieldCollectionValidationException{
-
+    public void setParticipants(int maxParticipants, int minParticipants) throws DomainFieldValidationException{
+        String fieldName;
+        String errorMessage;
 
         if (!ParticipantsValidation.isValid(minParticipants)) {
-            String fieldName = "MinParticipants";
-            String errorMessage = "Min participants number must be greater than 0 and smaller then 1000";
+            fieldName = "MinParticipants";
+            errorMessage = "Min participants number must be greater than 0 and smaller then 1000";
 
-            if (this.isInstantiated()) {
-                throw new DomainFieldValidationException(fieldName, errorMessage);
-            } else {
-                fieldErrors.put(fieldName, errorMessage);
-            }
+            handleError(fieldName,errorMessage,constructorHelper);
         }
 
         if (!ParticipantsValidation.isValid(maxParticipants)) {
-            String fieldName = "MaxParticipants";
-            String errorMessage = "Max participants number must be greater than 0 and smaller then 1000";
+            fieldName = "MaxParticipants";
+            errorMessage = "Max participants number must be greater than 0 and smaller then 1000";
 
-            if (this.isInstantiated()) {
-                throw new DomainFieldValidationException(fieldName, errorMessage);
-            } else {
-                fieldErrors.put(fieldName, errorMessage);
-            }
+            handleError(fieldName,errorMessage,constructorHelper);
         }
 
         if (maxParticipants < minParticipants) {
             ArrayList<String> fieldNames = new ArrayList<>();
             fieldNames.add("MinParticipants");
             fieldNames.add("MaxParticipants");
-            String errorMessage = "Min participants number must be smaller then Max participants number";
+            errorMessage = "Min participants number must be smaller then Max participants number";
 
-            if (this.isInstantiated()) {
-                throw new DomainFieldCollectionValidationException(fieldNames, errorMessage);
-            } else {
-                fieldErrors.put(fieldNames.get(0), errorMessage);
-                fieldErrors.put(fieldNames.get(1), errorMessage);
-            }
+            handleError(fieldNames.get(0),errorMessage,constructorHelper);
+            handleError(fieldNames.get(1),errorMessage,constructorHelper);
         }
 
         this.minParticipants = minParticipants;
@@ -107,11 +112,7 @@ public class TemplateModel implements IDomainObject {
             String fieldName = "Price";
             String errorMessage = "Price must be greater than 0";
 
-            if (this.isInstantiated()) {
-                throw new DomainFieldValidationException(fieldName, errorMessage);
-            } else {
-                fieldErrors.put(fieldName, errorMessage);
-            }
+            handleError(fieldName,errorMessage,constructorHelper);
         }
 
         this.price = price;
@@ -123,27 +124,9 @@ public class TemplateModel implements IDomainObject {
             String fieldName = "Id";
             String errorMessage = "Id must be positive";
 
-            if (this.isInstantiated()) {
-                throw new DomainFieldValidationException(fieldName, errorMessage);
-            } else {
-                fieldErrors.put(fieldName, errorMessage);
-            }
+            handleError(fieldName,errorMessage,constructorHelper);
         }
 
         this.id = id;
-    }
-
-
-    @Override
-    public void checkFieldErrors() throws DomainInstantiationException {
-        if (fieldErrors != null && !fieldErrors.isEmpty()) {
-            throw new DomainInstantiationException(fieldErrors);
-        }
-        fieldErrors = null;
-    }
-
-    @Override
-    public boolean isInstantiated() {
-        return fieldErrors == null;
     }
 }
