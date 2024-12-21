fdec79e64e53a320f956a7f2daa17d15d8bd0076
DanielJussel <daniel.jussel@students.fhv.at>
2024-12-21 18:13:03 +0100

    Container for templateListDTO und DTO

---
+++ src/main/java/fhv/team11/project/ems/events/transfer/EventTemplateDTOContainer.java
@@ -0,0 +1,15 @@
+package fhv.team11.project.ems.events.transfer;
+
+import fhv.team11.project.ems.events.repo.EventTemplate;
+import lombok.Getter;
+
+@Getter
+public class EventTemplateDTOContainer {
+    private EventTemplateDTO templateDTO;
+    private EventTemplateListDTO listDTO;
+
+    public EventTemplateDTOContainer(EventTemplateDTO templateDTO, EventTemplateListDTO listDTO) {
+        this.templateDTO = templateDTO;
+        this.listDTO = listDTO;
+    }
+}
